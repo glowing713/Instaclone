156e34b0b091e49b72997d0f9cb062b864ba3349
glowthem <glowing713@naver.com>
2020-07-20 15:31:54 +0900

    ➕ add toggleLike

---
+++ src/api/Post/toggleLike/toggleLike.js
@@ -0,0 +1,48 @@
+import { isAuthenticated } from "../../../middleware";
+import { makePrismaClientClass } from "prisma-client-lib";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    toggleLike: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { postId } = args;
+      const { user } = request;
+      try {
+        const existingLike = await prisma.$exists.like({
+          AND: [
+            {
+              user: {
+                id: user.id,
+              },
+            },
+            {
+              post: {
+                id: postId,
+              },
+            },
+          ],
+        });
+        if (existingLike) {
+          // TO DO
+        } else {
+          const newLike = await prisma.createLike({
+            user: {
+              connect: {
+                id: user.id,
+              },
+            },
+            post: {
+              connect: {
+                id: postId,
+              },
+            },
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+};
